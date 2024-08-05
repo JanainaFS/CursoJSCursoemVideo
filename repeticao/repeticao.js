2c32e43aea28c3ed52165ef5bd41a85d56a84b63
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2024-08-05 15:00:08 -0300

    Aula repeticao - for

--- repeticao/repeticao.js
+++ repeticao/repeticao.js
@@ -1,12 +1,16 @@
-/* var i = 1 
-while (i <= 6) {
+var i = 1 
+/* while (i <= 6) {
     console.log(`Passo ${i}`)
     i++
 } */
 
-var i = 1 
+/*var i = 1 
 do {
     console.log(`Passo ${i}`)
     i++
-}while (i <= 6)
-    
+}while (i <= 6) */
+console.log('Vai começar...')
+for(i = 1; i <=5; i++){
+    console.log(i)
+}
+console.log('FIM')
