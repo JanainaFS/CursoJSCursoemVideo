7cee5b1926bdcf6ae4d3e24ddb4fb8e62d589a2f
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2024-10-25 12:02:17 -0300

    Adicionando estilo na página

--- CursoJSPapito/js/app.js
+++ CursoJSPapito/js/app.js
@@ -1,7 +1,50 @@
+const LIST = [
+    {
+        id: 1,
+        nome: 'Yoda',
+        avatar: 'img/yoda.png'
+    },
+    {
+        id: 2,
+        nome: 'Luke',
+        avatar: 'img/luke.png'
+    },
+    {
+        id: 3,
+        nome: 'Leia',
+        avatar: 'img/leia.png'
+    },
+    {
+        id: 4,
+        nome: 'Hansolo',
+        avatar: 'img/hansolo.png'
+    },
+    {
+        id: 5,
+        nome: 'Vader',
+        avatar: 'img/vader.png'
+    },
+    {
+        id: 6,
+        nome: 'Chewbacca',
+        avatar: 'img/chewbacca.png'
+    },
+    {
+        id: 7,
+        nome: 'R2D2',
+        avatar: 'img/r2d2.png'
+    },
+    {
+        id: 8,
+        nome: 'C3pO',
+        avatar: 'img/c3po.png'
+    }
+]
 const App = new Vue({
     el: '#app',
     data: {
         title: 'Star Wars Lego',
-        userName: 'Janaina'
+        userName: 'Janaina',
+        characteres: LIST
     }
 })
